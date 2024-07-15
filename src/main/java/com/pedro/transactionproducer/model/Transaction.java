265eb26c3b5877c246c62c5d05280ca0c7ea7a2e
Pedro <pedriali.joao@proton.me>
2024-07-14 21:39:04 -0300

    Add Type header

--- src/main/java/com/pedro/transactionproducer/model/Transaction.java
+++ src/main/java/com/pedro/transactionproducer/model/Transaction.java
@@ -2,12 +2,13 @@ package com.pedro.transactionproducer.model;
 
 import lombok.Getter;
 import lombok.Setter;
+import lombok.ToString;
 
 import java.io.Serializable;
 
 @Getter
 @Setter
-
+@ToString
 public class Transaction implements Serializable {
     private AppEnum app;
     private String notificacao;
--- src/main/java/com/pedro/transactionproducer/service/MessageProducer.java
+++ src/main/java/com/pedro/transactionproducer/service/MessageProducer.java
@@ -2,18 +2,22 @@ package com.pedro.transactionproducer.service;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.pedro.transactionproducer.model.Transaction;
+import jakarta.jms.Session;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.jms.core.JmsTemplate;
+import org.springframework.messaging.MessageHeaders;
+import org.springframework.messaging.support.MessageBuilder;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor(onConstructor = @__(@Autowired))
+@Slf4j
 public class MessageProducer {
-    Logger logger = LoggerFactory.getLogger(MessageProducer.class);
 
     private final ObjectMapper mapper;
     private final JmsTemplate jmsTemplate;
@@ -23,8 +27,14 @@ public class MessageProducer {
 
     public void send(Transaction transaction) {
         try {
-            logger.info("Sending message :: {}", transaction.toString());
-            jmsTemplate.convertAndSend(queueName, transaction);
+            log.info("transaction content :: {}", transaction.toString());
+
+            jmsTemplate.convertAndSend(queueName, transaction, message -> {
+                message.setStringProperty("_type","com.pedro.financing.controller.v1.request.TransactionRequest");
+                log.info("Message :: {}", message);
+                return message;
+            });
+
         } catch (Exception e) {
             throw new RuntimeException(e);
         }
